b2dedfe1be591f7e32281826e73b6c01a8266f1a
LinZichuan <linzichuan12@163.com>
2016-04-22 15:46:06 +0800

    Add running script

---
+++ run.sh
@@ -0,0 +1,6 @@
+th load_data.lua $1 && 
+th scan.lua $1 
+# in the end of scan.lua, we should enter password th scp trueimage.t7 to host computer
+# then show trueimage to pick ice index, store index in icexxxx file
+# then run 'th getice.lua $1'
+#th getice.lua $1
